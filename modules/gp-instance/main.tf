f2f91189fb6d1009ac0a7da09da15e2cfe788d63
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2022-06-13 14:14:57 +0100

    remove set -xe

--- modules/gp-instance/main.tf
+++ modules/gp-instance/main.tf
@@ -38,7 +38,7 @@ data "template_file" "user_data" {
 ########################
 
 # Latest AMI for Amazon Linux 2 in this region
-# 31/07/2018: amzn2-ami-hvm-2.0.20180622.1-x86_64-gp2
+# 28/04/2022: amzn2-ami-hvm-2.0.20220426.0-x86_64-gp2	
 
 data "aws_ami" "ami_amzn2" {
   most_recent = true
